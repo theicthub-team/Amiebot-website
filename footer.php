8fc63461912208a0cb7dc9f870c45304f9845ab9
sadik061 <sadik061@gmail.com>
2021-04-03 18:37:17 +0600

    initilization

---
+++ footer.php
@@ -0,0 +1,48 @@
+<!--===================== Footer One area end =====================-->
+<!--===================== Footer Two area Start =====================-->
+<section class="footer_two">
+    <div class="container">
+        <h4>LINK <span>関連リンク</span></h4>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="footer_blog" style="background-image: url(img/footer_1.png);">
+                    <h3>POL</h3>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="footer_blog" style="background-image: url(img/footer_2.png);">
+                    <h3>POL求人サイト</h3>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="footer_blog" style="background-image: url(img/footer_3.png);">
+                    <h3>MobiControl</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--===================== Footer Two area end =====================-->
+<footer>
+    <p class="text-center">Copyright © Penetrate of Limits Co.,Ltd. All Rights Reserved.</p>
+</footer>
+<!-- JS here -->
+<script src="js/vendor/modernizr-3.5.0.min.js"></script>
+<script src="js/vendor/jquery-1.12.4.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/isotope.pkgd.min.js"></script>
+<script src="js/one-page-nav-min.js"></script>
+<script src="js/slick.min.js"></script>
+<script src="js/jquery.meanmenu.min.js"></script>
+<script src="js/ajax-form.js"></script>
+<script src="js/wow.min.js"></script>
+<script src="js/jquery.scrollUp.min.js"></script>
+<script src="js/imagesloaded.pkgd.min.js"></script>
+<script src="js/jquery.magnific-popup.min.js"></script>
+<script src="js/plugins.js"></script>
+<script src="js/main.js"></script>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,81 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Amie Blog site </title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="manifest" href="#">
+    <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
+    <!-- Place favicon.ico in the root directory -->
+    <!-- CSS here -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/animate.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/fontawesome-all.min.css">
+    <link rel="stylesheet" href="css/meanmenu.css">
+    <link rel="stylesheet" href="css/slick.css">
+    <link rel="stylesheet" href="css/default.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <link rel="stylesheet" href="css/custom.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <p class="header_top">人には人にしかできない仕事がある</p>
+            </div>
+        </div>
+    </div>
+    <!-- Navbar Menu -->
+    <header class="border_class">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <a class="navbar-brand" href="#"><img src="img/logo_amiebot.png" alt="logo"></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fas fa-bars"></i>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">
+                                <img src="img/icon/icon-1.png" alt="Icon">
+                                機能/特徴</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <img src="img/icon/icon-2.png" alt="Icon">
+                                事例</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <img src="img/icon/icon-3.png" alt="Icon">
+                                導入の流れ/<br />料金</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <img src="img/icon/icon-4.png" alt="Icon">
+                                お知らせ</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <img src="img/icon/icon-5.png" alt="Icon">
+                                コンテンツ</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <img src="img/icon/icon-6.png" alt="Icon">
+                                お問い合わせ</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </header>
