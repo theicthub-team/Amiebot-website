aed5a50f93654df614986e14fd2c1d301cfd34b3
rashik07 <saif67090707@gmail.com>
2021-04-03 20:29:24 -0700

    upload

--- index.php
+++ index.php
@@ -13,6 +13,85 @@
             </div>
 </section>
         <!-- Blank Images End -->
+          <!-- About us Area Start -->
+          <section class="position-relative about_us">
+            <div class="container">
+                <div class="golobal_header text-center">
+                    <h2>ABOUT</h2>
+                    <span>amieとは</span>
+                </div>
+                <div class="row justify-content-md-end">
+                    <div class="col-md-6  wrapper">
+                        <div class="left_site ">
+                            <img src="img/about.png" class="img" alt="about">
+                        </div>
+                    </div>
+                    <div class="col-md-6 ">
+                        <div class="about_content">
+                            <div class="button_group">
+                                <a href="#" class="btn "><i class="fas fa-check"></i>シナリオ不要</a>
+                                <a href="#" class="btn "><i class="fas fa-check"></i>事前準備不要</a>
+                            </div>
+                            <h2>
+                                窓口業務を超効率化！<br />
+                                最短1日で導入可能な<br />
+                                お手軽次世代ヘルプボット
+                            </h2>
+                            <p>
+                                amieはマニュアルやWebヘルプから自動的に学習する<br />
+                                【機械学習＋シナリオ型】のチャットボットです。<br />
+                                最初の学習に必要なことは、マニュアルやヘルプのファイル・URLの指定だけ。<br />
+                                一般的なチャットボット作成に必要なシナリオの準備や辞書の登録は必要ありません。<br />
+                                だから圧倒的なスピード感で導入が可能です。<br />
+                                初回学習時の正答率は6割！ログやアンケート、手動による単語登録などによ<br />りさらに精度を向上させることも可能です。
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row ">
+                    <div class="col-md-4 offset-md-1 order-xs-0 order-md-0">
+                        <div class="pt-80">
+                            <img src="img/about-2.png" alt="about-2.png" class="img">
+                        </div>
+                    </div>
+                    <div class="col-md-5 offset-md-1 order-xs-2 order-md-1">
+                        <div class="">
+                            <img src="img/about-3.png" alt="about-3.png" class="img">
+                        </div>
+                    </div>
+                
+                        <div class="col-md-6 order-xs-1 order-md-2">
+                            <div class="about_item">
+                                <h3>たったの2ステップで簡単学習</h3>
+                                <p>
+                                    チャットボットの導入において一番手間の掛かる初期導入時のシナリオ作成や、<br />
+                                    学習データの整形など不要！<br />
+                                    既にウェブヘルプがある場合は、学習したいページのトップURLの指定、<br />
+                                    マニュアルファイルやFAQデータファイルがあれば、ドラッグ＆ドロップするだけ。<br />
+                                    指定が完了したら「学習」ボタンを押すだけでチャットができる！<br />
+                                    チャットが完成したら精度のチェックをして表示したいウェブページにタグを追加す
+                                    るだけで導入可能です。
+                                </p>
+                            </div>
+                        </div> 
+                        <div class="col-md-6 order-xs-4 order-md-3">
+                            <div class="about_item">
+                                <h3>最短翌日提供！圧倒的な導入スピード</h3>
+                                <p>
+                                    Q&Aのデータ作成、専用フォーマットによる取り込み、辞書の登録、動作確認<br />
+                                    など、チャット作成だけでも一般的には1ヶ月以上かかり、そこから更にチューニン<br />
+                                    グ作業、サイトへの設置と合わせると導入には数ヶ月時間がかかります。amie<br />
+                                    ではそういった手間をかけずに、既存のファイル、ウェブサイトデータを元に自動で<br />
+                                    学習ができるため、管理サイトの提供後、数時間でチャットが完成します！<br />
+                                    チャット完成までを楽にスピーディにすることで導入が簡単になります。
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--================ About us Area End ================-->
 
 
 <?php include 'footer.php'; ?>
