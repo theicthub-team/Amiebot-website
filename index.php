c30595ae96bbc2a7337be8cd28eee90beeec9c43
rashik07 <saif67090707@gmail.com>
2021-04-03 19:29:56 -0700

    upload

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
 <?php include 'header.php'; ?>
 
-
+<section class="main_bg d-none d-sm-block" style="background-image: url(img/hero.png);"></section>
+<section class="main_bg d-block d-sm-none" style="background-image: url(img/mainv_sp.png);"></section>
+        <!-- Blank Images Start -->
+        <section class="pt-122 pb-145 blank_iamges">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-8">
+                        <img src="img/Rectangle 221.png" class="img">
+                    </div>
+                </div>
+            </div>
+</section>
+        <!-- Blank Images End -->
 
 
 <?php include 'footer.php'; ?>
