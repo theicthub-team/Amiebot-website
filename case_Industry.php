41d250900eb3ba4cd4446824a0572c5b5682425b
rashik07 <saif67090707@gmail.com>
2021-04-05 13:56:38 -0700

    upload

--- case_Industry.php
+++ case_Industry.php
@@ -1,7 +1,17 @@
 <?php include 'header.php'; ?>
 
+ <div class="container">
+    <p class="case_industry_heading_1 d-inline">
+        建設業・製造業
+    </p>
+    <p class="d-inline case_industry_heading_2 p-2" >社内特化</p>
+    <div class="case_industry_yellow ">
+        <p class="case_industry_yellow_bg p-1 " style="max-width:300px;">設備マニュアル・施工事例を簡単に検索</p>
+        <p class="case_industry_yellow_bg p-1">設備マニュアル・施工事</p>
+        <p class="case_industry_yellow_bg p-1">設備マニュアル・施工事例を簡単に検索</p>
 
-
+    </div>
+ </div>
 
 
 <?php include 'footer.php'?>
