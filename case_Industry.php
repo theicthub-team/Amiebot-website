1ee5558c333e1587f64a4d128023645a8eb02107
rashik07 <saif67090707@gmail.com>
2021-04-06 12:01:48 -0700

    upload

--- case_Industry.php
+++ case_Industry.php
@@ -1,68 +1,512 @@
 <?php include 'header.php'; ?>
 
- <div class="container">
-    <h2 class="case_industry_heading_1">
-        建設業・製造業
-    </h2>
-    <h2 class=" case_industry_heading_2 p-2" >社内特化</h2>
-    <div class="case_industry_yellow ">
-        <p class="case_industry_yellow_bg  " >設備マニュアル・施工事例を簡単に検索</p>
-        <p class="case_industry_yellow_bg  " >業界に特化した専門用語を簡単登録</p>
-        <p class="case_industry_yellow_bg " >社内共有情報・メルマガなどの簡単検索</p>
-
-
-
-    </div>
-
-    <div class=" case_industry_normal" >
-        <p class="case_industry_normal_1">専門用語がわからず問合せに時間がかかる</p>
-        <div class="case_industry_normal_div ">
-
+    <div class="container">
+        <!--start section1-->
+        <div class="case_industry_heading">
+            <h2 class="case_industry_heading_1">
+                建設業・製造業
+            </h2>
+            <h2 class=" case_industry_heading_2 p-2" >社内特化</h2>
         
-            <p class="case_industry_normal_2 p-2">
-                
-                エフロレッセンスの処理はどうすればいいの？など業界に特化した専門用語は、<br>
-                経験が浅い従業員には難しく、単語を覚えるところから時間がかかるが、<br>
-                辞書登録をして誰でも簡単に欲しい情報にたどり着くことができます。
-
-            </p>
         </div>
-    
-
-        <p class="case_industry_normal_1">社内の情報共有メールを見たいけど、埋もれてしまってなかなか見つからない<br>
-                                メルマガなどの有益な情報を活用したい
-                            
-        </p>
-        <div class="case_industry_normal_div ">
-
         
-            <p class="case_industry_normal_2 p-2">
-                
-            新製品のお知らせや事例などのメルマガなどを学習することで情報の有効活用が可能です。
+        <div class="case_industry_yellow ">
+            <p class="case_industry_yellow_bg  " >設備マニュアル・施工事例を簡単に検索</p>
+            <p class="case_industry_yellow_bg  " >業界に特化した専門用語を簡単登録</p>
+            <p class="case_industry_yellow_bg " >社内共有情報・メルマガなどの簡単検索</p>
 
-            </p>
+
+
         </div>
-        <p class="case_industry_normal_1">導入したばかりの機械の操作方法が分からない、確認したいが誰に聞けばいいかわからない、<br>
-                入社したばかりでどこを調べていいかわからない
-                            
-        </p>
-        <div class="case_industry_normal_div ">
 
+        <div class=" case_industry_normal" >
+            <p class="case_industry_normal_1">専門用語がわからず問合せに時間がかかる</p>
+            <div class="case_industry_normal_div ">
+
+            
+                <p class="case_industry_normal_2 p-2">
+                    
+                    エフロレッセンスの処理はどうすればいいの？など業界に特化した専門用語は、<br>
+                    経験が浅い従業員には難しく、単語を覚えるところから時間がかかるが、<br>
+                    辞書登録をして誰でも簡単に欲しい情報にたどり着くことができます。
+
+                </p>
+            </div>
         
-            <p class="case_industry_normal_2 p-2">
-                
-            操作マニュアルのドキュメントを学習することで誰でも欲しい情報にアクセスすることが可能です。
 
+            <p class="case_industry_normal_1">社内の情報共有メールを見たいけど、埋もれてしまってなかなか見つからない<br>
+                                    メルマガなどの有益な情報を活用したい
+                                
             </p>
-        </div>
-    </div>
-    
-    </div>
-    <div class="text-center mb-120 mt-190">
-            <a href="#" class="bg_button">一覧に戻る <i class="fas fa-caret-right"></i></a>
-    </div>
+            <div class="case_industry_normal_div ">
 
- </div>
+            
+                <p class="case_industry_normal_2 p-2">
+                    
+                新製品のお知らせや事例などのメルマガなどを学習することで情報の有効活用が可能です。
+
+                </p>
+            </div>
+            <p class="case_industry_normal_1">導入したばかりの機械の操作方法が分からない、確認したいが誰に聞けばいいかわからない、<br>
+                    入社したばかりでどこを調べていいかわからない
+                                
+            </p>
+            <div class="case_industry_normal_div ">
+
+            
+                <p class="case_industry_normal_2 p-2">
+                    
+                操作マニュアルのドキュメントを学習することで誰でも欲しい情報にアクセスすることが可能です。
+
+                </p>
+            </div>
+        </div>
+        
+        
+        <div class="text-center mb-120 mt-190">
+                <a href="#" class="bg_button">一覧に戻る <i class="fas fa-caret-right"></i></a>
+        </div>
+        <!--end section1-->
+        <!--start section2-->
+        <div class="case_industry_heading">
+            <h2 class="case_industry_heading_1">
+            ソリューション・サービス業
+            </h2>
+            <h2 class=" case_industry_heading_2 p-2" >社内</h2>
+            <h2 class=" case_industry_heading_2 p-2" >社外</h2>
+        
+        </div>
+        
+        <div class="case_industry_yellow ">
+            <p class="case_industry_yellow_bg  " >新規顧客からの製品の問合せ</p>
+            <p class="case_industry_yellow_bg  " >既存顧客からの使い方や事例の問合せ</p>
+            <p class="case_industry_yellow_bg " >在宅ワークでの有人チャット問合せ</p>
+
+
+
+        </div>
+
+        <div class=" case_industry_normal" >
+            <p class="case_industry_normal_1">製品の特長や価格など、FAQサイトに書いているような問合せが多い</p>
+            <div class="case_industry_normal_div ">
+
+            
+                <p class="case_industry_normal_2 p-2">
+                    
+                ホームページやその中のFAQサイトを学習することでよくある問合せを自動で回答することが可能です。
+
+                </p>
+            </div>
+        
+
+            <p class="case_industry_normal_1">製品マニュアルに書いているような質問が多い、<br>
+                        対応をする技術者の工数を削減したい
+                                
+            </p>
+            <div class="case_industry_normal_div ">
+
+            
+                <p class="case_industry_normal_2 p-2">
+                    
+                    製品マニュアルやメンバーサイトのページを学習することで、<br>
+                    回答を自動化することが可能です。複雑な質問を技術者が対応することで業務の効率化を図ります。
+                </p>
+            </div>
+            <p class="case_industry_normal_1">在宅ワークで電話の対応ができない、メールの対応が1.5倍に…
+                                
+            </p>
+            <div class="case_industry_normal_div ">
+
+            
+                <p class="case_industry_normal_2 p-2">
+                    
+                よくある質問はチャットで回答し、複雑な質問は有人チャット機能で手軽に返信ができます。
+
+                </p>
+            </div>
+        </div>
+        
+        
+        <div class="text-center mb-120 mt-190">
+                <a href="#" class="bg_button">一覧に戻る <i class="fas fa-caret-right"></i></a>
+        </div>
+        <!--end section2-->
+        <!--start section3-->
+        <div class="case_industry_heading">
+            <h2 class="case_industry_heading_1">
+            製造業
+            </h2>
+            <h2 class=" case_industry_heading_2 p-2" >社内特化</h2>
+            
+        
+        </div>
+        
+        <div class="case_industry_yellow ">
+            <p class="case_industry_yellow_bg  " >設備マニュアル検索チャット</p>
+            <p class="case_industry_yellow_bg  " >社内問合せ早わかり</p>
+            <p class="case_industry_yellow_bg " >後継者の育成としてチャットボットを活用</p>
+
+
+
+        </div>
+
+        <div class=" case_industry_normal" >
+            <p class="case_industry_normal_1">新しい設備の使い方がわからない</p>
+            <div class="case_industry_normal_div ">
+
+            
+                <p class="case_industry_normal_2 p-2">
+                    
+                    新しい設備の導入する際、製品マニュアルや運用マニュアルを<br>
+                    学習させる事で、誰でもすぐに使用が可能になります。使い慣れない初期の困りごとも即解決！
+
+                </p>
+            </div>
+        
+
+            <p class="case_industry_normal_1">パソコンが電源が入らない、システムの使い方が分からない、マニュアルを見ればわかるのに
+                                
+            </p>
+            <div class="case_industry_normal_div ">
+
+            
+                <p class="case_industry_normal_2 p-2">
+                    
+                    システムマニュアルや社内向けFAQサイトを学習するだけで、<br>
+                    いつでも欲しい情報を引き出すことが可能です。
+
+                </p>
+            </div>
+            <p class="case_industry_normal_1">人手不足で担当者に負担がかかっている、引継ぎを行いたいが時間も人もいない
+                                
+            </p>
+            <div class="case_industry_normal_div ">
+
+            
+                <p class="case_industry_normal_2 p-2">
+                    
+                    既にある運用マニュアルや過去の問合せ一覧を学習させて、チャットボットで回答ができるように！
+                </p>
+            </div>
+        </div>
+        
+        
+        <div class="text-center mb-120 mt-190">
+                <a href="#" class="bg_button">一覧に戻る <i class="fas fa-caret-right"></i></a>
+        </div>
+        <!--end section3-->
+        <!--start section4-->
+        <div class="case_industry_heading">
+            <h2 class="case_industry_heading_1">
+            教育機関（大学）
+            </h2>
+            <h2 class=" case_industry_heading_2 p-2" >学校外</h2>
+            
+        
+        </div>
+        
+        <div class="case_industry_yellow ">
+            <p class="case_industry_yellow_bg  " >入学試験のFAQ</p>
+            <p class="case_industry_yellow_bg  " >学校説明会</p>
+            
+        </div>
+
+        <div class=" case_industry_normal" >
+            <p class="case_industry_normal_1">WEBサイトのどこに書いているの入学試験の情報を知りたい</p>
+            <div class="case_industry_normal_div ">
+
+            
+                <p class="case_industry_normal_2 p-2">
+                    
+                    入学試験情報のWebページを学習させることで、] <br>
+                    必要なページを簡単に見つけることができ、ユーザーのストレスを低減、問い合わせを少なく！
+
+                </p>
+            </div>
+        
+
+            <p class="case_industry_normal_1">同じような問合せが多い、キャンパスの雰囲気が知りたい
+                                
+            </p>
+            <div class="case_industry_normal_div ">
+
+            
+                <p class="case_industry_normal_2 p-2">
+                    
+                    大学案内パンフレット、紹介ドキュメント、ウェブサイトを学習<br>
+                    探いキーワードの関連情報をピックアップ！
+
+                </p>
+            </div>
+            <p class="case_industry_normal_1">WEB説明会が増えていてコミュニケーションがうまく取れない、履修の仕方がわからない
+                                
+            </p>
+            <div class="case_industry_normal_div ">
+
+            
+                <p class="case_industry_normal_2 p-2">
+                    
+                    シラバスのデータを学習、多くの情報の中から欲しいものをピックアップするのが簡単！
+                </p>
+            </div>
+        </div>
+        
+        
+        <div class="text-center mb-120 mt-190">
+                <a href="#" class="bg_button">一覧に戻る <i class="fas fa-caret-right"></i></a>
+        </div>
+        <!--end section4-->
+        <!--start section5-->
+        <div class="case_industry_heading">
+            <h2 class="case_industry_heading_1">
+            保険
+            </h2>
+            <h2 class=" case_industry_heading_2 p-2" >社内</h2>
+            <h2 class=" case_industry_heading_2 p-2" >社外</h2>
+            
+        
+        </div>
+        
+        <div class="case_industry_yellow ">
+            <p class="case_industry_yellow_bg  " >保険商材のご案内</p>
+            <p class="case_industry_yellow_bg  " >契約内容の確認方法や保全お手続方法</p>
+            
+        </div>
+
+        <div class=" case_industry_normal" >
+            <p class="case_industry_normal_1">保険の商材が増えていて覚えるのが大変<br>
+                        新しいプランが出来た、以前のプランが無くなった、プランの内容が変更になった</p>
+            <div class="case_industry_normal_div ">
+
+            
+                <p class="case_industry_normal_2 p-2">
+                    
+                    対象のWEBサイトを学習することで、自動学習機能により最新の情報をすぐ導き出す事が可能に！
+
+                </p>
+            </div>
+        
+
+            <p class="case_industry_normal_1">既存顧客・新規顧客へ新しい商材を紹介したい<br>
+                    三大疾患に手厚い保証プラン、年齢制限があるプラン、貯蓄型のプランなど
+                                
+            </p>
+            <div class="case_industry_normal_div ">
+
+            
+                <p class="case_industry_normal_2 p-2">
+                    
+                顧客の求める最新プランをワード検索して提案が可能に！<br>
+                最も顧客に合った商材が導き出されます。探したいキーワードの関連情報をピックアップ！
+
+                </p>
+            </div>
+            <p class="case_industry_normal_1">同じような問合せが多い<br>
+                    車両保険って何？がん特約って何？申し込みの際に医師の検査は必要ですか？等
+                                
+            </p>
+            <div class="case_industry_normal_div ">
+
+            
+                <p class="case_industry_normal_2 p-2">
+                    
+                    FAQサイトを学習させることで面倒な問合せを大幅軽減！
+                </p>
+            </div>
+        </div>
+        
+        
+        <div class="text-center mb-120 mt-190">
+                <a href="#" class="bg_button">一覧に戻る <i class="fas fa-caret-right"></i></a>
+        </div>
+        <!--end section5-->
+        <!--start section6-->
+        <div class="case_industry_heading">
+            <h2 class="case_industry_heading_1">
+            管理部（人事・総務）
+            </h2>
+            <h2 class=" case_industry_heading_2 p-2" >社内特化</h2>
+            
+        
+        </div>
+        
+        <div class="case_industry_yellow ">
+            <p class="case_industry_yellow_bg  " >就業規則の問合せ</p>
+            <p class="case_industry_yellow_bg  " >社内規定の問合せ</p>
+            
+        </div>
+
+        <div class=" case_industry_normal" >
+            <p class="case_industry_normal_1">育児休暇の規定が分からない、年末調整の書き方が分からない、<br>
+                    領収書の申請方法が分からない、勤怠の修正方法が分からない、<br>
+                    交通費精算の方法が分からない</p>
+            <div class="case_industry_normal_div ">
+
+            
+                <p class="case_industry_normal_2 p-2">
+                    
+                    社内ポータル、ドキュメントを学習させるだけで誰でも簡単に欲しい情報を手に入れる事が可能に
+
+                </p>
+            </div>
+        
+
+            <p class="case_industry_normal_1">既存顧客・新規顧客へ新しい商材を紹介したい <br>
+                    三大疾患に手厚い保証プラン、年齢制限があるプラン、貯蓄型のプランなど
+                                
+            </p>
+            <div class="case_industry_normal_div ">
+
+            
+                <p class="case_industry_normal_2 p-2">
+                    
+                    修正された社内規定を学習するだけで簡単にチャットボットを更新することが可能
+
+                </p>
+            </div>
+            <p class="case_industry_normal_1">社内規定をスマホから見たい
+                                
+            </p>
+            <div class="case_industry_normal_div ">
+
+            
+                <p class="case_industry_normal_2 p-2">
+                    
+                    SNS連携が出来るのでどこにいても気軽に検索可能に！
+                </p>
+            </div>
+        </div>
+        
+        
+        <div class="text-center mb-120 mt-190">
+                <a href="#" class="bg_button">一覧に戻る <i class="fas fa-caret-right"></i></a>
+        </div>
+        <!--end section6-->
+        <!--start section7-->
+        <div class="case_industry_heading">
+            <h2 class="case_industry_heading_1">
+            顧客問合せの効率化
+            </h2>
+            <h2 class=" case_industry_heading_2 p-2" >社内</h2>
+            <h2 class=" case_industry_heading_2 p-2" >社外</h2>
+            
+        
+        </div>
+        
+        <div class="case_industry_yellow ">
+            <p class="case_industry_yellow_bg  " >新規顧客からの製品の問合せ</p>
+            <p class="case_industry_yellow_bg  " >既存顧客からの使い方や事例の問合せ</p>
+            
+        </div>
+
+        <div class=" case_industry_normal" >
+            <p class="case_industry_normal_1">製品の特長や価格など、FAQサイトに書いているような問合せが多い</p>
+            <div class="case_industry_normal_div ">
+
+            
+                <p class="case_industry_normal_2 p-2">
+                    
+                    ホームページやその中のFAQサイトを学習することでよくある問合せを自動で回答することが可能です。
+
+                </p>
+            </div>
+        
+
+            <p class="case_industry_normal_1">製品マニュアルに書いているような質問が多い、対応をする技術者の工数を削減したい
+                                
+            </p>
+            <div class="case_industry_normal_div ">
+
+            
+                <p class="case_industry_normal_2 p-2">
+                    
+                製品マニュアルやメンバーサイトのページを学習することで、回答を自動化することが可能です。<br>
+                複雑な質問を技術者が対応することで業務の効率化を図ります。
+
+                </p>
+            </div>
+            <p class="case_industry_normal_1">顧客からの問合せが増え、対応時間がかかっている
+                                
+            </p>
+            <div class="case_industry_normal_div ">
+
+            
+                <p class="case_industry_normal_2 p-2">
+                    
+                    FAQを読み込ませるだけで、１対１の対応から１対Nの対応になるので <br>
+                    少ない対応人数で問い合わせこなす事が可能。<br>
+                    ダッシュボード機能を活用することで問合せ内容が一目瞭然になり顧客分析としても活用可能に！ 
+                </p>
+            </div>
+        </div>
+        
+        
+        <div class="text-center mb-120 mt-190">
+                <a href="#" class="bg_button">一覧に戻る <i class="fas fa-caret-right"></i></a>
+        </div>
+        <!--end section7-->
+        <!--start section8-->
+        <div class="case_industry_heading">
+            <h2 class="case_industry_heading_1">
+            社内問合せの効率化
+            </h2>
+            <h2 class=" case_industry_heading_2 p-2" >社内特化</h2>
+            
+        
+        </div>
+        
+        <div class="case_industry_yellow ">
+            <p class="case_industry_yellow_bg  " >情報システム部使い方問合せ</p>
+            <p class="case_industry_yellow_bg  " >社内規定の問合せ</p>
+            
+        </div>
+
+        <div class=" case_industry_normal" >
+            <p class="case_industry_normal_1">パソコンの電源が入らない、システムの使い方が分からない、マニュアルを見ればわかるのに</p>
+            <div class="case_industry_normal_div ">
+
+            
+                <p class="case_industry_normal_2 p-2">
+                    
+                    システムマニュアルや社内向けFAQサイトを学習するだけで、いつでも欲しい情報を引き出すことが可能です。
+
+                </p>
+            </div>
+        
+
+            <p class="case_industry_normal_1">社内規定の追加や法改正による修正が多い、管理者が変わりルールが変更された、
+                                
+            </p>
+            <div class="case_industry_normal_div ">
+
+            
+                <p class="case_industry_normal_2 p-2">
+                    
+                修正された社内規定を学習するだけで簡単にチャットボットを更新することが可能。
+
+                </p>
+            </div>
+            <p class="case_industry_normal_1">社内の規定などを手軽にスマホで確認したい
+                                
+            </p>
+            <div class="case_industry_normal_div ">
+
+            
+                <p class="case_industry_normal_2 p-2">
+                    
+                    ブラウザやLINEなどのSNSからどこでも気軽に質問が可能に！ 
+                </p>
+            </div>
+        </div>
+        
+        
+        <div class="text-center mb-120 mt-190">
+                <a href="#" class="bg_button">一覧に戻る <i class="fas fa-caret-right"></i></a>
+        </div>
+        <!--end section7-->
+
+
+    </div>
 
 
 <?php include 'footer.php'?>
