4b9a85bef2d9cea7c67faefb25395c738a9666db
rashik07 <saif67090707@gmail.com>
2021-04-05 17:04:08 -0700

    upload

--- case_Industry.php
+++ case_Industry.php
@@ -1,16 +1,67 @@
 <?php include 'header.php'; ?>
 
  <div class="container">
-    <p class="case_industry_heading_1 d-inline">
+    <h2 class="case_industry_heading_1 d-inline">
         建設業・製造業
-    </p>
-    <p class="d-inline case_industry_heading_2 p-2" >社内特化</p>
+    </h2>
+    <h2 class="d-inline case_industry_heading_2 p-2" >社内特化</h2>
     <div class="case_industry_yellow ">
-        <p class="case_industry_yellow_bg p-1 col-sm-12" style="max-width:300px;">設備マニュアル・施工事例を簡単に検索</p>
-        <p class="case_industry_yellow_bg p-1 col-sm-12">設備マニュアル・施工事</p>
-        <p class="case_industry_yellow_bg p-1 col-sm-12">設備マニュアル・施工事例を簡単に検索</p>
+        <p class="case_industry_yellow_bg p-1 " style="width:300px">設備マニュアル・施工事例を簡単に検索</p>
+        <p class="case_industry_yellow_bg p-1 " style="width:279px">業界に特化した専門用語を簡単登録</p>
+        <p class="case_industry_yellow_bg p-1 " style="width:300px">社内共有情報・メルマガなどの簡単検索</p>
+
+
 
     </div>
+
+    <div class=" case_industry_normal" >
+        <p class="case_industry_normal_1">専門用語がわからず問合せに時間がかかる</p>
+        <div class="case_industry_normal_div ">
+
+        
+            <p class="case_industry_normal_2 p-2">
+                
+                エフロレッセンスの処理はどうすればいいの？など業界に特化した専門用語は、<br>
+                経験が浅い従業員には難しく、単語を覚えるところから時間がかかるが、<br>
+                辞書登録をして誰でも簡単に欲しい情報にたどり着くことができます。
+
+            </p>
+        </div>
+    
+
+        <p class="case_industry_normal_1">社内の情報共有メールを見たいけど、埋もれてしまってなかなか見つからない<br>
+                                メルマガなどの有益な情報を活用したい
+                            
+        </p>
+        <div class="case_industry_normal_div ">
+
+        
+            <p class="case_industry_normal_2 p-2">
+                
+            新製品のお知らせや事例などのメルマガなどを学習することで情報の有効活用が可能です。
+
+            </p>
+        </div>
+        <p class="case_industry_normal_1">導入したばかりの機械の操作方法が分からない、確認したいが誰に聞けばいいかわからない、<br>
+                入社したばかりでどこを調べていいかわからない
+                            
+        </p>
+        <div class="case_industry_normal_div ">
+
+        
+            <p class="case_industry_normal_2 p-2">
+                
+            操作マニュアルのドキュメントを学習することで誰でも欲しい情報にアクセスすることが可能です。
+
+            </p>
+        </div>
+    </div>
+    
+    </div>
+    <div class="text-center mb-120">
+            <a href="#" class="bg_button">一覧に戻る <i class="fas fa-caret-right"></i></a>
+    </div>
+
  </div>
 
 
