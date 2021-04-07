ce01e6ad1d9003b949de891efa0fa5f2fad024f6
rashik07 <saif67090707@gmail.com>
2021-04-06 18:05:19 -0700

    upload

---
+++ flow_price.php
@@ -0,0 +1,62 @@
+<?php include 'header.php'?>
+
+<div class="container">
+    <!--Flow section-->
+    <section>
+        <div class="text-center">
+            <h1 class="font-weight-bold" style="font-size:60px">FLOW</h1>
+            <h5 class="font-weight-normal" style="font-size:20px">導入の流れ</h5>
+            <h5 class="font-weight-normal " style="font-size:28px; margin-top:37px">最短翌日提供！圧倒的なスピード感で導入が可能です。</h5>
+
+        </div>
+        <div class="row">
+                    <div class="col-md-3 col-sm-12 mb-10">
+                        <div class="flow_item_text">
+                            <img src="img/flow_1.png" alt="start-page-image.png" class="img-fluid">
+                            <div class="flow_item_disc">
+                                <h4>デモ版申し込み</h4>
+                                <p>クラウドサーバーにてデモ環境のご用意</p>
+                                <a href="#" class="p-2 button_change d-block">3営業日</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-12 mb-10">
+                        <div class="flow_item_text">
+                            <img src="img/flow_2.png" alt="start-page-image.png" class="img-fluid">
+                            <div class="flow_item_disc">
+                                <h4>学習</h4>
+                                <p>
+                                   自社データで学習・チャットテスト
+                                </p>
+                                <a href="#" class="p-2 button_change d-block">3営業日</a>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-12 mb-10">
+                        <div class="flow_item_text">
+                            <img src="img/flow_3.png" alt="start-page-image.png" class="img-fluid">
+                            <div class="flow_item_disc">
+                                <h4>シナリオ追加</h4>
+                                <p>会話で不足している部分、ヘルプにない部分を追加</p>
+                                <a href="#" class="p-2 button_change d-block">3営業日</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-12 mb-10">
+                        <div class="flow_item_text hidden_shap">
+                            <img src="img/flow_4.png" alt="start-page-image.png" class="img-fluid">
+                            <div class="flow_item_disc">
+                                <h4>サイトに配置</h4>
+                                <p>チャットを使いたいサイトに配置</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+    </section>
+   
+    
+
+</div>
+
+<?php include 'footer.php'?>
