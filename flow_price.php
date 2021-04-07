821037d4b6f9cbc64e50af9f3aed8c8f77d16847
rashik07 <saif67090707@gmail.com>
2021-04-06 19:52:51 -0700

    upload

--- flow_price.php
+++ flow_price.php
@@ -9,7 +9,7 @@
             <h5 class="font-weight-normal " style="font-size:28px; margin-top:37px">最短翌日提供！圧倒的なスピード感で導入が可能です。</h5>
 
         </div>
-        <div class="row">
+        <div class="row mt-4">
                     <div class="col-md-3 col-sm-12 mb-10">
                         <div class="flow_item_text">
                             <img src="img/flow_1.png" alt="start-page-image.png" class="img-fluid">
@@ -52,7 +52,47 @@
                             </div>
                         </div>
                     </div>
+
+        </div>
+        <div class="amie_section pt-30 pb-90 pl-50 pr-50">
+
+        
+            <div class="text-center ">
+                <h5 class="font-weight-bold" style="font-size:28px">amieの導入が圧倒的にスピーディーな理由</h5>
+                <h5 class="font-weight-normal" style="font-size:20px">ー ご契約からスタートまでの期間の比較 ー</h5>
+                <hr>
+               
+            </div>
+            <div class="row">
+                <div class="col-md-6 col-sm-12 col-xs-12">
+                    <img src="img/features/Group591.png" alt="">
                 </div>
+                <div class="col-md-6 col-sm-12 col-xs-12">
+                    <h5 class="font-weight-bold" style="font-size:28px">学習時間が圧倒的に短いから、<br>
+                        導入までの期間に差がつく。</h5>
+                        <h5 class="font-weight-normal mt-30" style="font-size:16px">
+                            サービス開始初日にチャットが完成！<br>
+                            導入後の運用に時間をかけることが重要です。<br>
+                            運用までに時間をかけずに、導入した後から問い合わせ状況を確認しお客様の声を活かしてください。
+                        </h5>
+                    
+                </div>
+            </div>
+        </div>
+        <!-- price section start-->
+        <div class="text-center mt-100">
+            <h1 class="font-weight-bold" style="font-size:60px">Price</h1>
+            <span>料金</span>
+           
+            <div class="mt-100 mb-110 price_sec_2">
+                <h5 class="font-weight-bold" style="font-size:28px">機械学習型チャットボットでは圧倒的低価格を実現。</h5>
+                <h5 class="mt-40 font-weight-normal" style="font-size:16px">学習用のファイルと過去の会話データにより機械自ら学習を行い、ユーザーが会話をチャットに投げかけることにより精度が向上する「機械学習型」。<br>
+                    そんな機械学習型チャットボットの中でも、amieは圧倒的に始めやすい価格帯となっています。</h5>
+            </div>
+           
+
+        </div>
+
     </section>
    
     
